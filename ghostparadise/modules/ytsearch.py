3f2604995029b41517d7906e7fbab78b6b934133
Vihara2 <87529004+Vihara2@users.noreply.github.com>
2021-08-12 22:15:09 +0530

    Update ytsearch.py

--- ghostparadise/modules/ytsearch.py
+++ ghostparadise/modules/ytsearch.py
@@ -1,5 +1,5 @@
   
-# Ghost Paradise (Telegram bot project )
+# ghost paradise (Telegram bot project )
 # Copyright (C) 2021 Vihara Senindu
 
 # This program is free software: you can redistribute it and/or modify
