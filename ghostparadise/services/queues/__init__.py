df550aee59a9716fe6b6ab9ec7a0d187d92ab879
Vihara2 <87529004+Vihara2@users.noreply.github.com>
2021-08-12 21:32:08 +0530

    Update __init__.py

--- ghostparadise/services/queues/__init__.py
+++ ghostparadise/services/queues/__init__.py
@@ -1,8 +1,8 @@
 
-from GhostParadise.services.queues.queues import clear 
-from GhostParadise.services.queues.queues import get
-from GhostParadise.services.queues.queues import is_empty
-from GhostParadise.services.queues.queues import put
-from GhostParadise.services.queues.queues import task_done
+from ghostparadise.services.queues.queues import clear 
+from ghostparadise.services.queues.queues import get
+from ghostparadise.services.queues.queues import is_empty
+from ghostparadise.services.queues.queues import put
+from ghostparadise.services.queues.queues import task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
