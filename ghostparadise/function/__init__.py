f4194356b144f0efd3b882beaf337ae4c242224c
Vihara2 <87529004+Vihara2@users.noreply.github.com>
2021-08-12 13:57:30 +0530

    Create __init__.py

---
+++ ghostparadise/function/__init__.py
@@ -0,0 +1,6 @@
+
+from GhostParadise.function.admins import admins
+from GhostParadise.function.admins import get
+from GhostParadise.function.admins import set
+
+__all__ = ["set", "get", "admins"]
