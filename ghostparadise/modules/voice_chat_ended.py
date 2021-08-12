53759398fc0b073347cc5622a99574b6aaf2d3c6
Vihara2 <87529004+Vihara2@users.noreply.github.com>
2021-08-12 21:12:59 +0530

    Update voice_chat_ended.py

--- ghostparadise/modules/voice_chat_ended.py
+++ ghostparadise/modules/voice_chat_ended.py
@@ -2,8 +2,8 @@ from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ShadowMusic.services.callsmusic.callsmusic import remove
-from ShadowMusic.helpers.channelmusic import get_chat_id
+from ghostparadise.services.callsmusic.callsmusic import remove
+from ghostparadise.helpers.channelmusic import get_chat_id
 
 
 @Client.on_message(filters.voice_chat_ended)
