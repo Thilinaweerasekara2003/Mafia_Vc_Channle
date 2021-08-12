776fe642cf30fa893ad9e76436c641f262be8a55
Vihara2 <87529004+Vihara2@users.noreply.github.com>
2021-08-12 21:30:51 +0530

    Update converter.py

--- ghostparadise/services/converter/converter.py
+++ ghostparadise/services/converter/converter.py
@@ -18,7 +18,7 @@
 import asyncio
 from os import path
 
-from ShadowMusic.helpers.errors import FFmpegReturnCodeError
+from ghostparadise.helpers.errors import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
