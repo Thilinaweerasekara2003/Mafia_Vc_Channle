a39cdf9adf700c2bf3033df7729c88a3562b2e70
Vihara2 <87529004+Vihara2@users.noreply.github.com>
2021-08-12 21:29:24 +0530

    Update __init__.py

--- ghostparadise/services/callsmusic/__init__.py
+++ ghostparadise/services/callsmusic/__init__.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 
-from ShadowMusic import config
+from ghostparadise import config
 
 client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 run = client.run
