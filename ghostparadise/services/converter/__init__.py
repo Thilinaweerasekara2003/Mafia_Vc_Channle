160c08b157fb8193621ebde59769a63751972f26
Vihara2 <87529004+Vihara2@users.noreply.github.com>
2021-08-12 21:30:25 +0530

    Update __init__.py

--- ghostparadise/services/converter/__init__.py
+++ ghostparadise/services/converter/__init__.py
@@ -4,6 +4,6 @@ from os import mkdir
 if 'raw_files' not in listdir():
     mkdir('raw_files')
 
-from ShadowMusic.services.converter.converter import convert
+from ghostparadise.services.converter.converter import convert
 
 __all__ = ["convert"]
