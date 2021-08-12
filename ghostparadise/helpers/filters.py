34865803cf4acf122f99f743b1bfff72453b93fb
Vihara2 <87529004+Vihara2@users.noreply.github.com>
2021-08-12 21:43:50 +0530

    Update filters.py

--- ghostparadise/helpers/filters.py
+++ ghostparadise/helpers/filters.py
@@ -20,7 +20,7 @@ from typing import Union
 
 from pyrogram import filters
 
-from ShadowMusic.config import COMMAND_PREFIXES
+from ghostparadise.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
